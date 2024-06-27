621d4abfdaa51c66c81c306bb30e167e9528cc2a
AhmetinzmSec <picadroattack@outlook.com>
2024-06-27 16:35:57 +0300

    Add CatNvim conf

---
+++ .config/CatNvim/init.c
@@ -0,0 +1,39 @@
+#include "nvim.h"
+
+int luaopen_init(lua_State *L) {
+  char *lazypath = concat_str(get_xdg_home(1), "/lazy/lazy.nvim/");
+  if (os_isdir(lazypath) == false) {
+    system(concat_str("git clone --filter=blob:none "
+                      "https://github.com/folke/lazy.nvim.git --branch=stable ",
+                      lazypath));
+  }
+  opt("runtimepath", runtimepath_default(false), lazypath);
+  // clang-format off
+  require_setup("lazy", LUA_TABLE(
+    {
+      spec = {
+        {
+          'catppuccin/nvim',
+          name = 'catppuccin',
+          opts = {
+            color_overrides = {
+              mocha = {
+                base = '#101013',
+                mantle = '#000000',
+                crust = '#000000',
+              },
+            },
+          }
+        },
+        {
+          'LazyVim/LazyVim',
+          import = 'lazyvim.plugins',
+          opts = { colorscheme = 'catppuccin' }
+        }
+      },
+      install = {
+        colorscheme = { 'catppuccin' }
+      }
+    }));
+  return 1;
+}
