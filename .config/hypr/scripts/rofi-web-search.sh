100cd8305b6d0c73c4a9614203af6ed0e14375d6
AhmetinzmSec <picadroattack@outlook.com>
2024-06-27 15:58:59 +0300

    Wofi menus update

--- .config/hypr/scripts/rofi-web-search.sh
+++ .config/hypr/scripts/rofi-web-search.sh
@@ -40,10 +40,10 @@ gen_list() {
 
 main() {
   # Pass the list to rofi
-  platform=$( (gen_list) | rofi -dmenu -matching fuzzy -no-custom -location -theme ~/.config/rofi/launchers/type-1/style-7.rasi 0 -p "Search > " )
+  platform=$( (gen_list) | wofi -dmenu -matching fuzzy -no-custom -location -theme ~/.config/rofi/launchers/type-1/style-7.rasi 0 -p "Search > " )
 
   if [[ -n "$platform" ]]; then
-    query=$( (echo ) | rofi  -dmenu -matching fuzzy -location -theme ~/.config/rofi/launchers/type-1/style-7.rasi 0 -p "Query > " )
+    query=$( (echo ) | wofi  -dmenu -matching fuzzy -location -theme ~/.config/rofi/launchers/type-1/style-7.rasi 0 -p "Query > " )
 
     if [[ -n "$query" ]]; then
       url=${URLS[$platform]}$query
