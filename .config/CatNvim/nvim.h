0e903f42cfc311681d2609db22458f5eb1a77e95
AhmetinzmSec <98494934+AhmetinzmSec@users.noreply.github.com>
2024-06-28 22:58:24 +0300

    Sa

---
+++ .config/CatNvim/nvim.h
@@ -0,0 +1,197 @@
+// Reference:
+// https://github.com/neovim/neovim/blob/master/src/nvim/api/private/defs.h
+#ifndef NVIM_H
+#define NVIM_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define ARRAY_DICT_INIT KV_INITIAL_VALUE
+#define STRING_INIT                                                            \
+  { .data = NULL, .size = 0 }
+#define OBJECT_INIT                                                            \
+  { .type = kObjectTypeNil }
+#define ERROR_INIT                                                             \
+  { .type = kErrorTypeNone, .msg = NULL }
+#define kvec_t(type)                                                           \
+  struct {                                                                     \
+    size_t size;                                                               \
+    size_t capacity;                                                           \
+    type *items;                                                               \
+  }
+
+// Basic types
+typedef enum {
+  kErrorTypeNone = -1,
+  kErrorTypeException,
+  kErrorTypeValidation,
+} ErrorType;
+
+typedef struct {
+  ErrorType type;
+  char *msg;
+} Error;
+
+typedef struct object Object;
+typedef kvec_t(Object) Array;
+
+typedef bool Boolean;
+typedef int64_t Integer;
+typedef double Float;
+
+typedef struct {
+  char *data;
+  size_t size;
+} String;
+
+typedef struct key_value_pair KeyValuePair;
+typedef kvec_t(KeyValuePair) Dictionary;
+
+typedef enum {
+  kObjectTypeNil = 0,
+  kObjectTypeBoolean,
+  kObjectTypeInteger,
+  kObjectTypeFloat,
+  kObjectTypeString,
+  kObjectTypeArray,
+  kObjectTypeDictionary,
+  kObjectTypeLuaRef,
+  // EXT types, cannot be split or reordered, see #EXT_OBJECT_TYPE_SHIFT
+  kObjectTypeBuffer,
+  kObjectTypeWindow,
+  kObjectTypeTabpage,
+} ObjectType;
+
+// List of possible XDG variables
+typedef enum {
+  kXDGNone = -1,
+  kXDGConfigHome, ///< XDG_CONFIG_HOME
+  kXDGDataHome,   ///< XDG_DATA_HOME
+  kXDGCacheHome,  ///< XDG_CACHE_HOME
+  kXDGStateHome,  ///< XDG_STATE_HOME
+  kXDGRuntimeDir, ///< XDG_RUNTIME_DIR
+  kXDGConfigDirs, ///< XDG_CONFIG_DIRS
+  kXDGDataDirs,   ///< XDG_DATA_DIRS
+} XDGVarType;
+
+typedef int LuaRef;
+struct object {
+  ObjectType type;
+  union {
+    Boolean boolean;
+    Integer integer;
+    Float floating;
+    String string;
+    Array array;
+    Dictionary dictionary;
+    LuaRef luaref;
+  } data;
+};
+
+#endif // NVIM_API_PRIVATE_DEFS_H
+
+#define Dict(name) KeyDict_##name
+
+typedef struct {
+  Object scope;
+  Object win;
+  Object buf;
+} KeyDict_option;
+
+typedef struct {
+  Object desc;
+  Object expr;
+  Object script;
+  Object silent;
+  Object unique;
+  Object nowait;
+  Object noremap;
+  Object callback;
+  Object replace_keycodes;
+} KeyDict_keymap;
+
+#define True                                                                   \
+  (Object) { .type = kObjectTypeBoolean, .data.boolean = true }
+#define False                                                                  \
+  (Object) { .type = kObjectTypeBoolean, .data.boolean = false }
+
+extern char *get_xdg_home(const int);
+extern char *stdpaths_get_xdg_var(const XDGVarType idx);
+extern char *stdpaths_user_data_subpath(const char *fname);
+extern bool os_isdir(const char *name);
+extern char *runtimepath_default(bool clean_arg);
+void nvim_set_option_value(uint64_t channel_id, String name, Object value,
+                           Dict(option) * opts, Error *err);
+extern char *concat_str(const char *restrict str1, const char *restrict str2);
+void opt(char *k, char *old, char *new) {
+  KeyDict_option o = {};
+  Error e = ERROR_INIT;
+  char *x = concat_str(concat_str(old, ","), new);
+  Object v = {.type = kObjectTypeString,
+              .data.string = (String){x, strlen(x) - 1}};
+  nvim_set_option_value(0, (String){.data = k, .size = strlen(k)}, v, &o, &e);
+}
+extern int do_cmdline_cmd(const char *cmd);
+
+#define ms(x)                                                                  \
+  (String) { x, sizeof(x) / sizeof(char) - 1 }
+void nvim_set_keymap(uint64_t channel_id, String mode, String lhs, String rhs,
+                     Dict(keymap) * opts, Error *err);
+#define map(m, l, r)                                                           \
+  do {                                                                         \
+    KeyDict_keymap o = {.noremap = True, .silent = True};                      \
+    Error e = ERROR_INIT;                                                      \
+    nvim_set_keymap(0, ms(m), ms(l), ms(r), &o, &e);                           \
+  } while (0)
+
+#define fs(x)                                                                  \
+  (String) { x, strlen(x) }
+
+void nvim_set_keymap(uint64_t channel_id, String mode, String lhs, String rhs,
+                     Dict(keymap) * opts, Error *err);
+
+#define fmap(m, lhs, rhs, i)                                                   \
+  do {                                                                         \
+    KeyDict_keymap o = {.noremap = True, .silent = True};                      \
+    Error e = ERROR_INIT;                                                      \
+    char l[] = lhs, r[] = rhs;                                                 \
+    sprintf(l, l, i), sprintf(r, r, i);                                        \
+    nvim_set_keymap(0, fs(m), fs(l), fs(r), &o, &e);                           \
+  } while (0)
+
+#define i(x)                                                                   \
+  (Object) { .type = kObjectTypeInteger, .data.integer = x }
+#define s(x)                                                                   \
+  (Object) {                                                                   \
+    .type = kObjectTypeString, .data.string = (String) {                       \
+      x, sizeof(x) / sizeof(char) - 1                                          \
+    }                                                                          \
+  }
+
+extern void nvim_set_var(String name, Object value, Error *err);
+#define g(k, v)                                                                \
+  do {                                                                         \
+    Error e = ERROR_INIT;                                                      \
+    nvim_set_var((String){.data = k, .size = sizeof(k) / sizeof(char) - 1}, v, \
+                 &e);                                                          \
+  } while (0)
+
+#define o(k, v)                                                                \
+  do {                                                                         \
+    KeyDict_option o = {};                                                     \
+    Error e = ERROR_INIT;                                                      \
+    nvim_set_option_value(                                                     \
+        0, (String){.data = k, .size = sizeof(k) / sizeof(char) - 1}, v, &o,   \
+        &e);                                                                   \
+  } while (0)
+
+typedef struct lua_State lua_State;
+#define LUA_TABLE(...) #__VA_ARGS__
+#define require_setup(m, s)                                                    \
+  do {                                                                         \
+    do_cmdline_cmd("lua require('" m "').setup" s);                            \
+  } while (0)
---
+++ .local/state/CatNvim/lazy/readme/doc/luvit-meta.md
@@ -0,0 +1,112 @@
+# Luvit-Meta
+
+This project is a collection of definition files for the framework [Luvit](https://github.com/Luvit/Luvit). The definitions provide types for all modules currently shipped in Luvit, the classes, methods and their arguments and returns; it does as well provide descriptions whereever possible, either by scraping the Luvit-docs, Nodejs docs, luv docs or manually written in some places.
+
+Some of the definitions are scraped off of code/docs and then some manual work is done to figure types out. For those the used scraper is located under [scrapers](/scrapers), the script outputs to a file in the same directory. From there it is manually copied over to the actual library definition.
+Other definitions -and most of them- are completely manually written, as it was required to figure the logic out in order to figure the types.
+
+### Cool, any examples?
+
+Here are some samples of it working:
+
+https://user-images.githubusercontent.com/38175840/169595790-80305ae3-c4fc-478a-af59-6bfa2acb2635.mp4
+
+
+https://user-images.githubusercontent.com/38175840/169615756-f9946531-d943-4942-9124-f819783bc180.mp4
+
+
+https://user-images.githubusercontent.com/38175840/169663522-5aed02c1-1308-49d6-8d63-3de23a872ef7.mp4
+
+
+### Where does this work?
+
+The definitions use the [Sumneko's Lua Language Server (LLS)](https://github.com/LuaLS/lua-language-server/) annotations, therefor it should work on any editor that support LLS, such as [VSCode](https://marketplace.visualstudio.com/items?itemName=sumneko.lua) and Neovim.
+
+### How to install it?
+
+#### On Visual Studio Code:
+
+  1. Install [this plugin](https://marketplace.visualstudio.com/items?itemName=sumneko.lua) then reload editor.
+    
+  2. Open the workspace you want to enable this in.
+  3. Press `Ctrl + Shift + P`, a prompt will open.
+  4. Type "Open addon manager" and press enter.
+
+  ![image](https://user-images.githubusercontent.com/38175840/222934323-e3f92383-98d1-4799-bc29-80453b78af84.png)
+
+  5. Search for `Luvit` then press `Enable`.
+
+  ![image](https://user-images.githubusercontent.com/38175840/222933683-fcf54e36-3b46-4ce0-b12a-3210e2c4f44e.png)
+
+  6. The definitions should now be enabled for that workspace.
+     Typing something like `local fs = require'fs'` will show you auto-completion.
+
+
+#### Manually, for other editors:
+
+  1. Install [LLS]((https://github.com/LuaLS/lua-language-server/)) for your editor.
+  2. Configure your meta directory.
+  3. [Click here](https://github.com/Bilal2453/luvit-meta/archive/refs/heads/release.zip) to download the definitions; alternatively clone this repository.
+  4. Extract the ZIP file to your meta directory/move the folder there.
+  5. Should now be ready. If not, check the editor plugin's manual for doing this.
+
+### What modules have definitions?
+
+Built-in Modules:
+
+- [x] buffer
+- [x] childprocess
+- [x] codec
+- [x] core
+- [x] dgram
+- [x] dns
+- [x] env
+- [x] fs
+- [x] helpful
+- [x] hooks
+- [x] http
+- [x] http-codec
+- [x] http-header
+- [x] https
+- [x] json
+- [x] los
+- [x] lpeg
+- [x] net
+- [x] path
+- [x] pathjoin
+- [x] pretty-print
+- [x] process
+- [x] querystring
+- [x] re
+- [x] readline
+- [x] repl
+- [x] require
+- [x] resource
+- [x] stream
+- [x] thread
+- [x] timer
+- [x] tls
+- [x] url
+- [x] ustring
+- [x] utils
+- [x] uv
+- [ ] miniz
+- [ ] luvi
+- [ ] rex
+- [ ] openssl
+- [ ] WinSVC
+
+Popular libraries that are not built-in:
+
+- [x] coro-http
+- [ ] coro-net
+- [ ] coro-channel
+- [ ] coro-wrapper
+- [ ] coro-fs
+- [ ] coro-spawn
+
+## License
+
+This exhausting project is licensed under Apache License 2.0, see [LICENSE](/LICENSE) for more information. Feel free to fork and/or PR changes.
+
+<!-- vim: set ft=markdown: -->
